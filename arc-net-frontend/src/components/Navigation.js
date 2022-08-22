1a24c43c65a5cd3a75c08f830fd70ff939973176
Van Huynh <vthuynh@cpp.edu>
2022-08-22 01:53:04 -0700

    add navbar in homepage

--- arc-net-frontend/src/components/Navigation.js
+++ arc-net-frontend/src/components/Navigation.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/globe.png";
 
 function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <img alt="logo" src={logo} style={{ width: 50, height: 50 }} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <LinkContainer to="/login">
               <Nav.Link>Login</Nav.Link>
             </LinkContainer>
--- arc-net-frontend/src/pages/Home.js
+++ arc-net-frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Row>
+      <Col
+        md={6}
+        className="d-flex flex-direction-column align-items-center justify-content-center"
+      >
+        <div>
+          <h1>Share the world with your friend</h1>
+          <p>Chat app lets you connect with everyone</p>
+          <LinkContainer to="/chat">
+            <Button variant="success">
+              Get Started
+              <i className="fas fa-comments home-message-icon"></i>
+            </Button>
+          </LinkContainer>
+        </div>
+      </Col>
+      <Col md={6} className="home__bg"></Col>
+    </Row>
+  );
 }
 
 export default Home;
