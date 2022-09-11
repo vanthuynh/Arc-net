89bd05624d21701bdd3ae4fe5407e1e25d63a980
Van Huynh <vthuynh@cpp.edu>
2022-09-11 14:53:53 -0700

    Implement Signup.js

--- arc-net-frontend/src/pages/Signup.js
+++ arc-net-frontend/src/pages/Signup.js
@@ -1,7 +1,66 @@
 import React from "react";
+import { Col, Container, Form, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Signup.css";
+import botImg from "../assets/bot.jpeg";
+
+function validateImg() {}
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <Container>
+      <Row>
+        <Col
+          md={7}
+          className="d-flex align-items-center justify-content-center flex-direction-column"
+        >
+          <Form style={{ width: "80%", maxWidth: 500 }}>
+            <h1 className="text-center">Create Account</h1>
+            <div className="signup-profile-pic__container">
+              <img src={botImg} className="signup-profile-pic" />
+              <label htmlFor="image-upload" className="image-upload-label">
+                <i className="fas fa-plus-circle add-picture-icon"></i>
+              </label>
+              <input
+                type="file"
+                id="image-upload"
+                hidden
+                accept="image/png, image/jpeg"
+                onChange={validateImg}
+              />
+            </div>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Your name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placehoder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Create account
+            </Button>
+
+            <div className="py-4">
+              <p className="text-center">
+                Already have an account? <Link to="/signup">Login</Link>
+              </p>
+            </div>
+          </Form>
+        </Col>
+        <Col md={5} className="signup__bg"></Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Signup;
