495d1aa158397146d4750d9f8b81710aac2fd31f
Van Huynh <vthuynh@cpp.edu>
2022-09-11 17:07:57 -0700

    Establish connection to MongoDB

---
+++ arc-net-backend/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.1gdpoay.mongodb.net/arc-net?retryWrites=true&w=majority`,
+  () => {
+    console.log("connected to mongodb");
+  }
+);
--- arc-net-backend/server.js
+++ arc-net-backend/server.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+require("./connection");
+
 const server = require("http").createServer(app);
 const PORT = 5001;
 const io = require("socket.io")(server, {
