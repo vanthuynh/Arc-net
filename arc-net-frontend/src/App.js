dbcf20270fa79538faa6fc37261463e16d8da8c5
Van Huynh <vthuynh@cpp.edu>
2022-09-21 18:31:14 -0700

    handle navigation when user already logged in or signed up

--- arc-net-frontend/src/App.js
+++ arc-net-frontend/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,13 +8,19 @@ import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
 
 function App() {
+  const user = useSelector((state) => state.user);
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        {/* hide login page when users already signed up or logged in */}
+        {!user && (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </>
+        )}
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
--- arc-net-frontend/src/components/Navigation.js
+++ arc-net-frontend/src/components/Navigation.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, Container, NavDropdown, Button } from "react-bootstrap";
+import { useLogoutUserMutation } from "../services/appApi";
+import { useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/globe.png";
 
 function Navigation() {
+  const user = useSelector((state) => state.user);
+  const [logoutUser] = useLogoutUserMutation();
+  async function handleLogout(e) {
+    e.preventDefault();
+    await logoutUser(user);
+
+    // navigate back to home page
+    window.location.replace("/");
+  }
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -13,23 +24,50 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
+            {/* hide login button if user already logged in */}
+            {!user && (
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            )}
             <LinkContainer to="/chat">
               <Nav.Link>Chat</Nav.Link>
             </LinkContainer>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+
+            {/* have dropdown only when there is user logged in */}
+            {user && (
+              <NavDropdown
+                title={
+                  <>
+                    <img
+                      src={user.picture}
+                      style={{
+                        width: 30,
+                        height: 30,
+                        marginRight: 10,
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                      }}
+                    />
+                    {user.name}
+                  </>
+                }
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  <Button variant="danger" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
